8f9263bc20539cab12c95442f92ceb374f799346
Abdoulaye <soumare1@hotmail.fr>
2013-02-25 00:26:36 +0100

    Validation initiale

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Apprentissage::Application.config.secret_token = 'b0b8bc1f850efcca264a2ef2711239363f28d525f4bf6238a0a9a9c229cabd253615b52a91cbbc3426429a1a6a735b505e8eec6f81a78a37b0540906fa92d1f9'
